809820c3ea789231518abc754283e7bf764b5e72
Daw-Ran Liou <dawran6@gmail.com>
2019-06-20 15:17:11 -0700

    Injuct get user fx

--- cheffy/src/app/auth/events.cljs
+++ cheffy/src/app/auth/events.cljs
@@ -1,5 +1,6 @@
 (ns app.auth.events
-  (:require [re-frame.core :as rf :refer [reg-event-fx reg-event-db after]]))
+  (:require [re-frame.core :as rf :refer [reg-event-fx reg-event-db after reg-cofx]]
+            [cljs.reader :refer [read-string]]))
 
 (def cheffy-user-key "cheffy-user")
 
@@ -15,11 +16,12 @@
 (def set-user-interceptors [(after set-user-ls!)])
 (def remove-user-interceptors [(after remove-user-ls!)])
 
-(reg-event-db
- :load-user-ls!
- (fn [db _]
-   (let [auth (js->clj (.getItem js/localStorage cheffy-user-key))]
-     (assoc-in db [:auth] auth))))
+(reg-cofx
+ :local-store-user
+ (fn [cofx _]
+   (assoc cofx :local-store-user
+          (read-string
+           (.getItem js/localStorage cheffy-user-key)))))
 
 (reg-event-fx
  :log-in
