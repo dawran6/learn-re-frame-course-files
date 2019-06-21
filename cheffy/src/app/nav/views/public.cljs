9c6067dcdf89c8676b2a97adb7a42617e54066b1
Daw-Ran Liou <dawran6@gmail.com>
2019-06-21 07:33:30 -0700

    Add router dispatches for public pages

--- cheffy/src/app/nav/views/public.cljs
+++ cheffy/src/app/nav/views/public.cljs
@@ -1,6 +1,7 @@
 (ns app.nav.views.public
   (:require [re-frame.core :as rf]
             [app.nav.views.nav-item :refer [nav-item]]
+            [app.router :as router]
             ["@smooth-ui/core-sc" :refer [Box]]))
 
 (defn public
@@ -9,19 +10,19 @@
         nav-items [
                    {:id :recipes
                     :name "Recipes"
-                    :href "#recipes"
+                    :href (router/path-for :recipes)
                     :dispatch #(rf/dispatch [:set-active-nav :recipes])}
                    {:id :become-a-chef
                     :name "Chef"
-                    :href "#become-a-chef"
+                    :href (router/path-for :become-a-chef)
                     :dispatch #(rf/dispatch [:set-active-nav :become-a-chef])}
                    {:id :sign-up
                     :name "Sign up"
-                    :href "#sign-up"
+                    :href (router/path-for :sign-up)
                     :dispatch #(rf/dispatch [:set-active-nav :sign-up])}
                    {:id :log-in
                     :name "Log in"
-                    :href "#log-in"
+                    :href (router/path-for :log-in)
                     :dispatch #(rf/dispatch [:set-active-nav :log-in])}]]
     [:> Box {:display "flex"
              :justify-content "flex-end"
